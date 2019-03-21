ae40c9cac29f5cd3f7fcbd1b4b56aea0c9f86ee3
katrinasilver <katrina.agustin@gmail.com>
2019-03-21 15:02:15 -0700

    add appointment list

--- src/components/App.js
+++ src/components/App.js
@@ -10,27 +10,41 @@ export default class App extends Component {
     super(props)
 
     this.state = {
-      name: 'Katrina'
+      appointments: []
     }
   }
 
-  componentDidMount() {
-    
+  async componentDidMount() {
+    try {
+      const response = await fetch('./data.json')
+      const appointments = await response.json()
+      appointments.map(item => item)
+
+      this.setState({ appointments })
+
+    } catch(err) {
+      console.error(err)
+    }
   }
 
   render() {
+
     return (
      <main className="page bg-white" id="petratings">
       <div className="container">
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              {
-                this.state.name
-              }
+    
               <AddAppointments />
               <SearchAppointments />
-              <ListAppointments />
+              {/* <ListAppointments appointment={this.state.appointments} /> */}
+
+              {
+                this.state.appointments.map((appt, i) => 
+                  <ListAppointments key={i} id={i} {...appt} />
+                )
+              }
             </div>
           </div>
         </div>
